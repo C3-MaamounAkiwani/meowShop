028a90f22df15671a263e17664bd624e507e9e93
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-09-08 20:21:07 +0300

    modify Purchase name and country  unique

--- backend/db/models/purchase.js
+++ backend/db/models/purchase.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-// its done 
 const purchaseModel = new mongoose.Schema({
     address: { type: String, required: true },
     city: { type: String, required: true },
     phoneNumber: { type: Number, required: true },
-    country: { type: String, required: true, unique: true },
+    country: { type: String, required: true },
 });
 
-module.exports = mongoose.model("purchase", purchaseModel);
+module.exports = mongoose.model("Purchase", purchaseModel);
